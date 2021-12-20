76018c7009b335861657668cfcda39128144eb50
Renate Hauser <renate.hauser@students.unibe.ch>
2021-12-20 17:09:46 +0100

    script to create file for annotating a goldstandard

---
+++ scripts/create_gs_file.py
@@ -0,0 +1,104 @@
+import csv
+import spacy
+import argparse
+
+
+class TsvWriter():
+
+    def __init__(self, infile, outfile):
+        self.infile = infile
+        self.outfile = outfile
+        self.nlp = spacy.load('en_core_web_md')
+        self.rev_sent_id = 50000000
+        self.resp_sent_id = 80000000
+
+    # get structure: {doc_id: [[(rev_sentID, rev_sent, None/''), (resp_sentID, resp_sent, None/'')][(rev_sentID, rev_sent, None/''), ('', '', '')
+    def split_sentences(self):
+        segmented = {}
+        with open(self.infile, 'r', encoding='utf-8') as infile:
+            reader = csv.reader(infile, delimiter=',')
+            for line in reader:
+                # get relevant data from the line
+                document_id = line[0]
+                review = line[5]
+                response = line[6]
+
+                # for each doc start sentence ids with the next number divisible by 10
+                if (self.rev_sent_id % 10) != 0:
+                    rest = 10 - (self.rev_sent_id % 10)
+                    self.rev_sent_id += rest
+                if (self.resp_sent_id % 10) != 0:
+                    rest = 10 - (self.rev_sent_id % 10)
+                    self.resp_sent_id += rest
+
+
+                rev_doc = self.nlp(review)
+                resp_doc = self.nlp(response)
+                rev_sents, resp_sents = [], []
+
+                # do the sentence segmentation of the review doc
+                for sent in rev_doc.sents:
+                    sent_tup = (self.rev_sent_id, sent.text, '')
+                    rev_sents.append(sent_tup)
+                    # increment sentence ID
+                    self.rev_sent_id += 1
+                # do the sentence segmentation of the response doc
+                for sent in resp_doc.sents:
+                    sent_tup = (self.resp_sent_id, sent.text, '')
+                    resp_sents.append(sent_tup)
+                    # increment sentence ID
+                    self.resp_sent_id += 1
+
+                # pad with empty tuples so that documents can be aligned nicely in the tsv
+                if len(rev_sents) != len(resp_sents):
+                    # pad review document
+                    if len(rev_sents) < len(resp_sents):
+                        i = len(rev_sents)
+                        while i < len(resp_sents):
+                            rev_sents.append(('', '', ''))
+                            i += 1
+                    # pad response document
+                    elif len(rev_sents) > len(resp_sents):
+                        i = len(resp_sents)
+                        while i < len(rev_sents):
+                            resp_sents.append(('', '', ''))
+                            i += 1
+
+                segmented[document_id] = zip(rev_sents, resp_sents)
+        return segmented
+
+    def write_tsv(self):
+        with open(self.outfile, 'w', newline='', encoding='utf-8') as outf:
+            # headers
+            tsv_writer = csv.writer(outf, delimiter='\t')
+            tsv_writer.writerow(['DOCID', 'REVIEW SENTID', 'REVIEW SENT', 'REVIEW AUID', 'RESPONSE SENTID', 'RESPONSE SENT', 'RESPONSE AUID'])
+
+            # get dictionary with doc ids and segmented sentences
+            segmented = self.split_sentences()
+
+            # concatenate doc id, review sentence and response sentence to form a new row
+            for doc_id, sents in segmented.items():
+                for sent in sents:
+                    row = (doc_id,) + sent[0] + sent[1]
+                    tsv_writer.writerow(row)
+                tsv_writer.writerow('')
+
+# command line interface
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('infile', help="path to the raw goldstandard file")
+    parser.add_argument('outfile', help="path to the new goldstandard tsv file")
+
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+    tsv_writer = TsvWriter(infile=args.infile, outfile=args.outfile)
+    tsv_writer.write_tsv()
+
+
+if __name__ == '__main__':
+    main()
