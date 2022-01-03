ecbff598d0472c294bff578d85e6c3cc4341eb3d
Renate Hauser <renate.hauser@students.unibe.ch>
2022-01-03 14:56:44 +0100

    added basic script to create similarity matrices for sentence-split input file

--- scripts/similarity.py
+++ scripts/similarity.py
@@ -1,38 +1,35 @@
 import argparse
+import json
+import csv
 from collections import Counter
 from nltk.util import ngrams
 
 
 class Similarity():
 
-    def read_data(self, filename):
-        # TODO: read in doc
-        # read into which format?
-        # {rev: [sent1, sent2, sent3], resp: [sent1, sent2]}
-        ...
+    def __init__(self, order):
+        self.order = order
 
     def calculate_similarities(self, doc):
-        # TODO: per document, produce matrix
-        # get doc {rev: [sent1, sent2, sent3], resp: [sent1, sent2]}
-        longer = doc['rev'] if len(doc['rev']) >= len(doc['resp']) else doc['resp']
-        shorter = doc['rev'] if len(doc['rev']) < len(doc['resp']) else doc['resp']
+        # get doc in following format: {rev: {sent_id1: sent1, sent_id2: sent2}, resp: {resp_sentid1: resp_sent1, resp_sentid2: resp_sent2}}
+        similarity_matrix = {}  # {rev_id1: {resp_id1: 0.0, resp_id2: 0.1}, rev_id2: {resp_id1: 0.2, resp_id2: 0.3}}
+        for rev_id, rev_sent in doc['rev'].items():
+            similarities = {}
+            for resp_id, resp_sent in doc['resp'].items():
+                similarity = self.chrF(rev_sent, resp_sent)
+                similarities[resp_id] = similarity
+            similarity_matrix[rev_id] = similarities
+        return similarity_matrix  # {rev_id1: {resp_id1: 0.0, resp_id2: 0.1}, rev_id2: {resp_id1: 0.2, resp_id2: 0.3}}
 
-        for seg_l in longer:
-            for seg_s in shorter:
-                similarity = self.chrF(seg_l, seg_s)
-
-        # TODO: format?
-        return similarity_matrix
-
-    def create_ngrams(self, sent, n):
-        ngram_tups = ngrams(sent, n)
+    def create_ngrams(self, sent):
+        ngram_tups = ngrams(sent, self.order)
         grams = ["".join(tup) for tup in ngram_tups]
         return grams
 
-    def chrF(self, ref_sent, hyp_sent, order):
+    def chrF(self, ref_sent, hyp_sent):
 
-        ref_ngrams = self.create_ngrams(ref_sent, order)
-        hyp_ngrams = self.create_ngrams(hyp_sent, order)
+        ref_ngrams = self.create_ngrams(ref_sent)
+        hyp_ngrams = self.create_ngrams(hyp_sent)
 
         ref_ngram_counts = Counter(ref_ngrams)
         hyp_ngram_counts = Counter(hyp_ngrams)
@@ -49,31 +46,76 @@ class Similarity():
             elif hyp_ngram_counts[ngram] > ref_ngram_counts[ngram]:
                 overlap_count += ref_ngram_counts[ngram]
 
-        chrP = overlap_count / len(hyp_ngrams)
-        chrR = overlap_count / len(ref_ngrams)
+        chrP = overlap_count / len(hyp_ngrams) if len(hyp_ngrams) > 0 else 0
+        chrR = overlap_count / len(ref_ngrams) if len(ref_ngrams) > 0 else 0
 
-        chrF = 2 * ((chrP * chrR) / (chrP + chrR))
-        return chrF
+        if chrP == 0 and chrR == 0:
+            return 0
+        else:
+            chrF = 2 * ((chrP * chrR) / (chrP + chrR))
+            return chrF
 
 
-    def write_data(self, filename):
-        # TODO: save in tsv format
-        ...
+def read_data(filename):
+    # {doc1: {rev: {sent_id1: sent1, sent_id2: sent2}, resp: {resp_sentid1: resp_sent1, resp_sentid2: resp_sent2}}}
+    data = {}
+    with open(filename, 'r', encoding='utf-8') as inf:
+        reader = csv.reader(inf, delimiter='\t')
+        for line in reader:
+            # skip empty dividing lines
+            if not line:
+                continue
+            # get relevant data from line
+            document_id = str(line[0])
+            rev_sent_id = str(line[1]) if line[1] else None
+            rev_sent = line[2]
+            resp_sent_id = str(line[4]) if line[4] else None
+            resp_sent = line[5]
+            # add all sentences of one document to the dictionary entry of the document ID
+            if document_id in data.keys():
+                if rev_sent_id is not None:
+                    data[document_id]['rev'][rev_sent_id] = rev_sent
+                if resp_sent_id is not None:
+                    data[document_id]['resp'][resp_sent_id] = resp_sent
+            # if a new document starts, initialize new dict for document ID
+            else:
+                data[document_id] = {'rev': {rev_sent_id: rev_sent}, 'resp': {resp_sent_id: resp_sent}}
+    return data
+
+
+def write_data(filename, data):
+    # TODO: write in tsv format
+    # write in json format
+    with open(filename, 'w', encoding='utf-8') as outf:
+        json.dump(data, outf, indent=4)
 
 
 # command line interface
 def parse_args():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('infile', help="path to the raw goldstandard file")
-    parser.add_argument('outfile', help="path to the new goldstandard tsv file")
+    parser.add_argument('infile', help="path to the sentence segmented file")
+    parser.add_argument('outfile', help="path to the file with the similarity matrices")
+    parser.add_argument('--out-format', choices=['tsv', 'json'], default='json')
+    parser.add_argument('--ngram-order', default=6)  # default as in Popovic, 2015
 
     args = parser.parse_args()
     return args
 
 
 def main():
-    ...
+    args = parse_args()
+
+    sim = Similarity(order=args.ngram_order)
+    data = read_data(
+        args.infile)  # {doc1: {rev: {sent_id1: sent1, sent_id2: sent2}, resp: {resp_sentid1: resp_sent1, resp_sentid2: resp_sent2}}}
+
+    corpus_with_sim = {}
+    for doc_id, doc in data.items():
+        similarity_matrix = sim.calculate_similarities(doc)
+        corpus_with_sim[doc_id] = similarity_matrix
+
+    write_data(args.outfile, corpus_with_sim)
 
 
 if __name__ == '__main__':
