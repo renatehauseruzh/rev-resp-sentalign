71f3d75096592d6c41455b35dea5dadc8061ddcf
Renate Hauser <renate.hauser@students.unibe.ch>
2022-01-17 16:40:30 +0100

    renamed output files

--- scripts/evaluate/align_st_th01-06.sh
+++ scripts/evaluate/align_st_th01-06.sh
@@ -32,14 +32,14 @@ python3 $alignment/align.py $data/goldstandard/rev-resp-goldstandard_empty_115.c
 
 echo "Create alignments with nli model"
 
-python3 $alignment/align.py $data/goldstandard/rev-resp-goldstandard_empty_115.csv $evaluation/st_para_th01.json --threshold 0.1 --scoring "sentence embedding" --model "sentence-transformers/nli-mpnet-base-v2"
+python3 $alignment/align.py $data/goldstandard/rev-resp-goldstandard_empty_115.csv $evaluation/st_nli_th01.json --threshold 0.1 --scoring "sentence embedding" --model "sentence-transformers/nli-mpnet-base-v2"
 
-python3 $alignment/align.py $data/goldstandard/rev-resp-goldstandard_empty_115.csv $evaluation/st_para_th02.json --threshold 0.2 --scoring "sentence embedding" --model "sentence-transformers/nli-mpnet-base-v2"
+python3 $alignment/align.py $data/goldstandard/rev-resp-goldstandard_empty_115.csv $evaluation/st_nli_th02.json --threshold 0.2 --scoring "sentence embedding" --model "sentence-transformers/nli-mpnet-base-v2"
 
-python3 $alignment/align.py $data/goldstandard/rev-resp-goldstandard_empty_115.csv $evaluation/st_para_th03.json --threshold 0.3 --scoring "sentence embedding" --model "sentence-transformers/nli-mpnet-base-v2"
+python3 $alignment/align.py $data/goldstandard/rev-resp-goldstandard_empty_115.csv $evaluation/st_nli_th03.json --threshold 0.3 --scoring "sentence embedding" --model "sentence-transformers/nli-mpnet-base-v2"
 
-python3 $alignment/align.py $data/goldstandard/rev-resp-goldstandard_empty_115.csv $evaluation/st_para_th04.json --threshold 0.4 --scoring "sentence embedding" --model "sentence-transformers/nli-mpnet-base-v2"
+python3 $alignment/align.py $data/goldstandard/rev-resp-goldstandard_empty_115.csv $evaluation/st_nli_th04.json --threshold 0.4 --scoring "sentence embedding" --model "sentence-transformers/nli-mpnet-base-v2"
 
-python3 $alignment/align.py $data/goldstandard/rev-resp-goldstandard_empty_115.csv $evaluation/st_para_th05.json --threshold 0.5 --scoring "sentence embedding" --model "sentence-transformers/nli-mpnet-base-v2"
+python3 $alignment/align.py $data/goldstandard/rev-resp-goldstandard_empty_115.csv $evaluation/st_nli_th05.json --threshold 0.5 --scoring "sentence embedding" --model "sentence-transformers/nli-mpnet-base-v2"
 
-python3 $alignment/align.py $data/goldstandard/rev-resp-goldstandard_empty_115.csv $evaluation/st_para_th06.json --threshold 0.6 --scoring "sentence embedding" --model "sentence-transformers/nli-mpnet-base-v2"
+python3 $alignment/align.py $data/goldstandard/rev-resp-goldstandard_empty_115.csv $evaluation/st_nli_th06.json --threshold 0.6 --scoring "sentence embedding" --model "sentence-transformers/nli-mpnet-base-v2"
